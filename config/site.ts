e1f59fb0c7f515488674e57bbaab051a86c3a702
Garrett Humert <57776596+Arxci@users.noreply.github.com>
2024-04-19 17:49:21 -0500

    setup first post

--- config/site.ts
+++ config/site.ts
@@ -1,9 +1,13 @@
 export const siteConfig: SiteConfig = {
 	name: 'Blog App',
 	description: 'Blog app created with NextJS',
+	url: 'https://example.com',
+	author: 'Garrett Humbert',
 }
 
 interface SiteConfig {
 	name: string
 	description: string
+	url: string
+	author: string
 }
